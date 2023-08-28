2fecdb2c600f2b4fe57a9479ff777ad843634c0c
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-08-28 14:29:15 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l&+ui+1g)5iy2$lw)q_3j3q$pp2v7)9^23npusn$eyyhmr$s-g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['172.105.41.115']
 
@@ -121,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
